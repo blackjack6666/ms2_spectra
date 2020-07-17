d007d880a1e4e109b821e74aa74d54b6ac222d91
blackjack <xshao8@uic.edu>
2020-07-17 11:46:34 -0700

    prosit precusor compare

--- prosit_precusor_compare.py
+++ prosit_precusor_compare.py
@@ -7,65 +7,85 @@ import numpy as np
 from scipy import spatial
 import pickle as ppp
 
-peptide_list = ppp.load(open('C:/Users/gao lab computer/PycharmProjects/extend_different_species/PXD001723_ext_pep_list.p','rb'))  # target peptide list
-peptide_list = [each for each in peptide_list if len(each)<=30]
-"""
-pep_file = 'D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/peptide.tsv'  # tsv or dta file
-csv_out = 'D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/PXD001723_ext_prosit.csv'
-prosit_csv = prosit_csv_output(peptide_list,csv_out,pep_file) # output csv file subject to prosit prediction
-"""
 
-msp_file_path = 'D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/myPrositLib.msp'
-prosit_info_dict = msp_reader(msp_file_path)  # info dictionary with predicted ms2 m/z intensity array
 
-# prosit dictionary key and value change
-new_prosit_info_dict = defaultdict(list)  # make pep_seq as key, (m/z array, int array) as value
-for each in prosit_info_dict:
-    pep_seq, mz_array, int_array = prosit_info_dict[each][0], prosit_info_dict[each][-2], prosit_info_dict[each][-1]
-    new_prosit_info_dict[pep_seq].append((mz_array,int_array))
+def prosit_csv_input(pep_file,csv_out,peptide_list):
+    """
+    write peptide seq and charge info into a csv file subject to prosit predict
+    :param pep_file:
+    :param csv_out:
+    :param peptide_list:
+    :return: csv file
+    """
+    prosit_csv = prosit_csv_output(peptide_list,csv_out,pep_file) # output csv file subject to prosit prediction
+    return prosit_csv
 
-print (new_prosit_info_dict)
 
-# target peptides file and spectra info
-psm_path = 'D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/psm.tsv'
-pep_file_spec_dict_of_dict = peptide_file_spec_dict_of_dict(psm_path)
-print ('1')
-target_pep_file_spec_dict_of_dict = {each:pep_file_spec_dict_of_dict[each] for each in peptide_list}
-print (target_pep_file_spec_dict_of_dict)
+def msp_info_dict_gen(msp_file_path):
+    """
+    get info from prosit output msp file into dictionary
+    :param msp_file_path:
+    :return: dictionary with peptide as key and list of m/z array and int array tuple as value
+    """
+    prosit_info_dict = msp_reader(msp_file_path)  # info dictionary with predicted ms2 m/z intensity array
 
-# compare similarity between prosit prediction and acquired precusor ms2
-ms2_dict_of_dict = ppp.load(open('D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/PXD001723_ms2_dict_of_dict_7_13.p','rb'))
-cos_sim_score_dict = defaultdict(list)
-for each_pep in new_prosit_info_dict:
-    print (each_pep)
+    # prosit dictionary key and value change
+    new_prosit_info_dict = defaultdict(list)  # make pep_seq as key, (m/z array, int array) as value
+    for each in prosit_info_dict:
+        pep_seq, mz_array, int_array = prosit_info_dict[each][0], prosit_info_dict[each][-2], prosit_info_dict[each][-1]
+        new_prosit_info_dict[pep_seq].append((mz_array,int_array))
+    return new_prosit_info_dict
 
-    # file_spec_list dict {file:[spec1, spec2],}
-    file_spec_list_dict = target_pep_file_spec_dict_of_dict[each_pep]
 
-    # get m/z, int array for peptide seq from prosit
-    for each_tuple in new_prosit_info_dict[each_pep]:
-        predicted_mz_array, predicted_int_array = each_tuple
-        v1 = np.zeros(290000)  # m/z from 200-3000, 100 bins, make a int vector to compare similarity.
+def target_pep_files_spectra_gen(peptide_list,psm_path):
+    # target peptides file and spectra info
 
-        for each_pred_mz, each_pred_int in zip(predicted_mz_array,predicted_int_array):
-            v1[int((each_pred_mz-200)*100)] += each_pred_int
+    pep_file_spec_dict_of_dict = peptide_file_spec_dict_of_dict(psm_path)
+    #print ('1')
+    target_pep_file_spec_dict_of_dict = {each:pep_file_spec_dict_of_dict[each] for each in peptide_list}
+    return target_pep_file_spec_dict_of_dict
 
-        for each_file in file_spec_list_dict:
-            for each_spec in file_spec_list_dict[each_file]:
-                print (each_file,each_spec)
-                precusor_mz_array, precusor_int_array = ms2_dict_of_dict['F:/XS/c_elegans/PXD001723'+'\\'+each_file+'_clean.ms2'][each_spec][-2:]
-                max_int = max(precusor_int_array)
-                # normalize the intensity array to be compatible with prosit result
-                precusor_int_array = [float(each)/max_int for each in precusor_int_array]
-                v2 = np.zeros(290000)
 
-                for each_mz, each_int in zip(precusor_mz_array,precusor_int_array):
-                    v2[int((each_mz-200)*100)] += each_int
-                cos_sim = 1-spatial.distance.cosine(v1,v2)
-                print (cos_sim)
-                cos_sim_score_dict[each_pep].append((each_file+'_'+str(each_spec),cos_sim))
-print (cos_sim_score_dict)
+def cosine_similarity_compare(new_prosit_info_dict, target_pep_file_spec_dict_of_dict, ms2_dict_of_dict):
+    """
+    compare cosine similarity between prosit prediciton and acquired precursor
+    :param new_prosit_info_dict:
+    :param target_pep_file_spec_dict_of_dict:
+    :param ms2_dict_of_dict: returned by function ms2_info_dict_generator, a dictionary with file name as key, info_dict as value
+    :return:
+    """
 
+    cos_sim_score_dict = defaultdict(list)
+    for each_pep in new_prosit_info_dict:
+        print (each_pep)
+
+        # file_spec_list dict {file:[spec1, spec2],}
+        file_spec_list_dict = target_pep_file_spec_dict_of_dict[each_pep]
+
+        # get m/z, int array for peptide seq from prosit
+        for each_tuple in new_prosit_info_dict[each_pep]:
+            predicted_mz_array, predicted_int_array = each_tuple
+            v1 = np.zeros(290000)  # m/z from 200-3000, 100 bins, make a int vector to compare similarity.
+
+            for each_pred_mz, each_pred_int in zip(predicted_mz_array,predicted_int_array):
+                v1[int((each_pred_mz-200)*100)] += each_pred_int
+
+            for each_file in file_spec_list_dict:
+                for each_spec in file_spec_list_dict[each_file]:
+                    print (each_file,each_spec)
+                    precusor_mz_array, precusor_int_array = ms2_dict_of_dict['F:/XS/c_elegans/PXD001723'+'\\'+each_file+'_clean.ms2'][each_spec][-2:]
+                    max_int = max(precusor_int_array)
+                    # normalize the intensity array to be compatible with prosit result
+                    precusor_int_array = [float(each)/max_int for each in precusor_int_array]
+                    v2 = np.zeros(290000)
+
+                    for each_mz, each_int in zip(precusor_mz_array,precusor_int_array):
+                        v2[int((each_mz-200)*100)] += each_int
+                    cos_sim = 1-spatial.distance.cosine(v1,v2)
+                    print (cos_sim)
+                    cos_sim_score_dict[each_pep].append((each_file+'_'+str(each_spec),cos_sim))
+    print (cos_sim_score_dict)
+    return cos_sim_score_dict
 def ms2_info_dict_generator(psm_tsv_path, target_pep_list, ms2_path, pickle_saved_path = None):
     """
     write ms2 info of target peptides in a dictionary of dictionary, {file:{spec1:(),spec2(),...}}
@@ -93,3 +113,24 @@ def ms2_info_dict_generator(psm_tsv_path, target_pep_list, ms2_path, pickle_save
 
     return ms2_dict_of_dict
 
+if __name__=='__main__':
+    msp_file_path = 'D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/myPrositLib.msp'
+    msp_info_dict = msp_info_dict_gen(msp_file_path)
+
+    peptide_list = ppp.load(
+        open('C:/Users/gao lab computer/PycharmProjects/extend_different_species/PXD001723_ext_pep_list.p',
+             'rb'))  # target peptide list
+    peptide_list = [each for each in peptide_list if len(each) <= 30] # peptides longer than 30aa are not compatible with prosit
+    print (len(peptide_list))
+
+    ms2_dict_of_dict = ppp.load(open('D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/PXD001723_ms2_dict_of_dict_7_13.p','rb'))
+
+    psm_path = 'D:/data/ext_evo_pj/gb_ext_search_7_11_PXD001723/psm.tsv'
+    target_pep_file_spec_dict_of_dict = target_pep_files_spectra_gen(peptide_list,psm_path)
+
+    print (target_pep_file_spec_dict_of_dict)
+    cosine_score_dict = cosine_similarity_compare(msp_info_dict,target_pep_file_spec_dict_of_dict,ms2_dict_of_dict)
+    print (len(cosine_score_dict))
+
+    high_conf_ext_pep_list = [(pep,each_tu[0],each_tu[1]) for pep in cosine_score_dict for each_tu in cosine_score_dict[pep] if each_tu[1]>0.5]
+    print (high_conf_ext_pep_list)
