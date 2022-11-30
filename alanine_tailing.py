c559e835889234000b7f9176ffcf2f235a5dd6e2
blackjack <xshao8@uic.edu>
2022-11-29 20:05:53 -0600

    poly alanine

--- alanine_tailing.py
+++ alanine_tailing.py
@@ -21,8 +21,8 @@ def add_alanine(fasta_in, fasta_out, num_alanine=5):
             for i in range(len(block)-1):
                 f_out.write(seq[block[i]:block[i+1]]+'\n')
             for i in range(num_alanine):
-                new_id = prot+'_'+str(i+1)+'A'
-                new_seq = seq+'A'*(i+1)
+                new_id = prot+'_'+str(i+1)+'L'
+                new_seq = seq+'L'*(i+1)
                 new_block = range(0, len(new_seq) + 60, 60)
                 f_out.write('>' + descript_dict[prot][0] + '|' + new_id + '|' + descript_dict[prot][1] +' '+ str(i+1)+'Alanines'+'\n')
                 for i in range(len(new_block) - 1):
@@ -31,10 +31,10 @@ def add_alanine(fasta_in, fasta_out, num_alanine=5):
 
 
 if __name__ == '__main__':
-    # fastain = 'F:/alanine_tailing/20220709/gfp_truncation.txt'
-    # fastaout = 'F:/alanine_tailing/20220709/gfp_truncation_alanines.fasta'
-    # add_alanine(fastain,fastaout)
-    combined_prot_tsv = 'F:/alanine_tailing/20220709/ecoli_db_search/combined_protein.tsv'
-    info_dict = combined_proteintsv_map(combined_prot_tsv)
-    for each in info_dict:
-        print (each, len(info_dict[each]))
+    fastain = 'F:/alanine_tailing/2022_10_28/arfa_truncation.txt'
+    fastaout = 'F:/alanine_tailing/2022_10_28/arfa_truncation_leucines.fasta'
+    add_alanine(fastain,fastaout)
+    # combined_prot_tsv = 'F:/alanine_tailing/20220709/ecoli_db_search/combined_protein.tsv'
+    # info_dict = combined_proteintsv_map(combined_prot_tsv)
+    # for each in info_dict:
+    #     print (each, len(info_dict[each]))
